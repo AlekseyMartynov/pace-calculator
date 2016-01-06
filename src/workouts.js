ae34e040ee604a2a265b219854683465261c5b84
AlekseyMartynov <bclexa@gmail.com>
2016-01-06 12:58:57 +0300

    adjust tempo intervals workout, misc text edits

--- src/workouts.js
+++ src/workouts.js
@@ -3,9 +3,8 @@
 // https://www.mcmillanrunning.com/articlePages/article/3
 // https://www.mcmillanrunning.com/articlePages/page/21
 
-// TODO add short comments for each workout to display in tooltip
-
-var SEC_PER_HOUR = 3600;
+var SEC_PER_HOUR = 3600,
+    STAMINA_WARMUP = "Warm-up for 10-20 min.";
 
 module.exports = {
 
@@ -18,7 +17,7 @@ module.exports = {
         },
         {
             name: "Long",
-            info: "At least 1 h. To improve endurance. Optionally finish the last 10 min at a medium-hard effort.",
+            info: "At least 1 h. To improve endurance. Optionally fast-finish the last 10 min at a medium-hard effort.",
             racePace: [ 3.75 * SEC_PER_HOUR, 8 * SEC_PER_HOUR ],
             hrPercent: [ 0.6, 0.85 ]
         },
@@ -33,20 +32,20 @@ module.exports = {
     stamina: [
         {
             name: "Steady-State",
-            info: "25-90 min. Lactate-clearance pace. Warm-up for 10-20 min.",
+            info: "25-90 min. Lactate clearance pace. " + STAMINA_WARMUP,
             racePace: [ 1.25 * SEC_PER_HOUR, 2.5 * SEC_PER_HOUR ],
             hrPercent: [ 0.83, 0.87 ]
         },
         {
             name: "Tempo",
-            info: "10-40 min. Lactate-threshold pace. Comfortably-hard effort. Warm-up for 10-20 min.",
+            info: "10-40 min. Lactate threshold pace. Comfortably-hard effort. " + STAMINA_WARMUP,
             racePace: [ 2 / 3 * SEC_PER_HOUR, 1.25 * SEC_PER_HOUR ],
             hrPercent: [ 0.85, 0.9 ]
         },
         {
             name: "Tempo Intervals",
-            info: "8-15 min with 2-5 min recovery. Your current 5-10K pace. Medium-hard effort. Warm-up for 10-20 min.",
-            racePace: [ 0.5 * SEC_PER_HOUR, 1 * SEC_PER_HOUR ],
+            info: "2-15 min run with quarter-duration jog between. Above lactate threshold. Medium-hard effort. " + STAMINA_WARMUP,
+            racePace: [ 5/12 * SEC_PER_HOUR, 1 * SEC_PER_HOUR ],
             hrPercent: [ 0.87, 0.92 ]
         }
     ]
