8dfb4007d90dd4ddff5a2260809d3c67432923b6
Aleksey Martynov <bclexa@gmail.com>
2016-01-12 15:18:24 +0300

    fix select appearance in old androids

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 "use strict";
 
 $(function() {
+    applyBrowserHacks();
+    
     ko.applyBindings(require("./viewModel"));
     
     $("body").tooltip({ 
@@ -10,9 +12,6 @@ $(function() {
         container: "body",
         placement: "auto"
     });    
-
-    if(/windows phone|iemobile/i.test(navigator.userAgent))
-        $("head").append("<style> @-ms-viewport { width: 340px; } </style>");
 });
 
 if(window.cordova)
@@ -30,4 +29,18 @@ function onBackButton(e) {
     } else {
         navigator.app.exitApp();
     }
+}
+
+function applyBrowserHacks() {
+    var ua = navigator.userAgent,
+        winPhone = /windows phone|iemobile/i.test(ua),
+        android = !winPhone && /android/i.test(ua);
+        
+    if(winPhone)
+        $("head").append("<style> @-ms-viewport { width: 340px; } </style>");
+        
+    // http://getbootstrap.com/getting-started/#support-android-stock-browser
+    // https://github.com/twbs/bootstrap/issues/11055#issuecomment-34422391
+    if(android && "WebkitAppearance" in document.documentElement.style && ua.indexOf("Chrome") < 0)
+        $("html").addClass("android-stock");
 }
